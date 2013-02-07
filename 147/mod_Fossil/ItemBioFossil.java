4ce33882941c09bda751cc60bb1ccbce6cec85b4
4f6f3b <mail.ptran@gmail.com>
2013-02-06 23:14:30 -0600

    BioFossil fix
    Using the right function now to place Biofossils and Stoneboards.
    Stoneboards still have an issue where they overlap.
    
    Don't know why the zh_TW.lang was changed...

--- 147/mod_Fossil/ItemBioFossil.java
+++ 147/mod_Fossil/ItemBioFossil.java
@@ -40,7 +40,7 @@ public class ItemBioFossil extends Item
     		}
     		return false;
     }*/
-    public boolean tryPlaceIntoWorld(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
+    public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
     {
         if (par3World.isRemote)
         {
